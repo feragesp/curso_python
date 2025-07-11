5d8cd82c4bf41a770f3cd993fd9a144558b26293
Fernando Aguilar <feraguilarespinosa@gmail.com>
2025-07-11 10:51:30 +0200

    Calculadora

---
+++ Ex_Calculadora/static/script.js
@@ -0,0 +1,33 @@
+const display = document.getElementById('display');
+let currentInput = '';
+let justCalculated = false;
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.dataset.value;
+
+        if (button.id === 'clear') {
+            currentInput = '';
+            display.textContent = '0';
+            justCalculated = false;
+        } else if (button.id === 'equals') {
+            sendToServer(currentInput);
+        } else {
+            if (justCalculated && !isNaN(value)){
+                //Si acabamos de calcular y ahora viene un nº, reiniciamos
+                currentInput = value;
+            } else {
+                currentInput += value;
+            }
+
+            display.textContent = currentInput;
+            justCalculated = false;
+        }
+    });
+});
+
+//Funcion de conexion Python y JavaScript
+function sendToServer(expression) {
+
+}
+
